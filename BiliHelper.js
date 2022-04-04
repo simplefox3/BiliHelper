f2b3f93ff59d72a44d8c13c35d5c58883b9455bd
SangKuanJi <sangkuanji@gmail.com>
2022-04-04 17:51:52 +0800

    Update BiliHelper.js
    支持 live

--- BiliHelper.js
+++ BiliHelper.js
@@ -1,18 +1,21 @@
 // ==UserScript==
 // @name        bilibili 自动网页全屏
-// @author      imbedinlove
-// @namespace   imbedinlove
-// @description bili 自动网页全屏
-// @version     1.25
-// @include     http://www.bilibili.com/video/av*
+// @author      sangkuanji
+// @namespace   nana_vao_script
+// @description 启用bilibili的html5播放器，自动宽屏、原生右键菜单
+// @version     1.29
+// @include     http://www.bilibili.com/video/BV*
+// @include     https://www.bilibili.com/video/BV*
+// @include     https://www.bilibili.com/video/av*
 // @include     http://bangumi.bilibili.com/anime/v/*
-// @include     http://live.bilibili.com/*
+// @include     https://bangumi.bilibili.com/anime/v/*
+// @include     https://live.bilibili.com/*
 // @run-at      document-start
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_deleteValue
 // ==/UserScript==
-'use strict';
+
 (function () {
     let url = GM_getValue('url');
     GM_deleteValue('url');
@@ -31,8 +34,13 @@
             localStorage.setItem('defaulth5', '1');
         }catch(e){}
         window.addEventListener('load', function () {
+            console.log("load wait success");
             this.$ = unsafeWindow.jQuery;
-            $('.bilibili-player-iconfont-web-fullscreen').click();
+            waitElement(function() { //等待#btn_comment_submit元素的加载
+                console.log("wait element, click element " + document.getElementsByClassName("bilibili-player-iconfont-web-fullscreen-off").length);
+                document.getElementsByClassName("bilibili-player-iconfont-web-fullscreen-off")[0].click();
+                console.log("click succes");
+            }, ".bilibili-player-iconfont-web-fullscreen-off");
             scrollToPlayer();
             let intervalId  = setInterval(function(){
                 if($('.bilibili-player-video-wrap video').length){
@@ -42,14 +50,39 @@
             },500);
         });
     }
-    
+
+    function waitElement(func,selector, times, interval) {
+        this.$ = unsafeWindow.jQuery;
+        var _times = times || -1, //100次
+            _interval = interval || 20, //20毫秒每次
+            _self = document.getElementsByClassName("bilibili-player-iconfont-web-fullscreen-off"),
+            _selector = selector, //选择器
+            _iIntervalID; //定时器id
+        if( _self.length != 0){ //如果已经获取到了，就直接执行函数
+            func && func.call(this);
+        } else {
+            console.log("times" + _times);
+            _iIntervalID = setInterval(function() {
+                if(!_times) { //是0就退出
+                    clearInterval(_iIntervalID);
+                }
+                _times <= 0 || _times--; //如果是正数就 --
+                _self = document.getElementsByClassName("bilibili-player-iconfont-web-fullscreen-off"); //再次选择
+                if( _self.length != 0) { //判断是否取到
+                    func && func.call(_self);
+                    clearInterval(_iIntervalID);
+                }
+            }, _interval);
+        }
+        return this;
+    }
     function scrollToPlayer(){
+        console.log("scrollTopPlayer");
         var player = $('#bilibiliPlayer');
         if($(window).scrollTop() === 0){
             $(window).scrollTop(player.offset().top + player.height() - $(window).height());
         }
     }
-    
     function setContextMenuHandler(){
         let contextMenuEvent = $._data( document.querySelector('.bilibili-player-video-wrap'), "events" ).contextmenu[0];
         let oldHandler = contextMenuEvent.handler;
